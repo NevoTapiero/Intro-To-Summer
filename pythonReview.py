647ac44d15df446488d62f0544d55e3ba77ee384
NevoTapiero <nevotap1234@gmail.com>
2022-07-24 18:50:48 +0300

    Update pythonReview.py
    (;

--- pythonReview.py
+++ pythonReview.py
@@ -1,2 +1,6 @@
 def create_youtube_video(title, description):
 	IshowSpeed = {"title": title, "description": description, "likes": 0, "dislikes": 0, comments:{}}
+	return IshowSpeed
+title = input("What is your title? ")
+description = input("what is your youtube video description? ")
+new_youtube_video = create_youtube_video(title, description)
